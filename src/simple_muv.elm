f8202d9a1181c6ee16cbbd3bb6ae06e949cefa1e
Alli <wonga10@gmail.com>
2018-10-31 20:44:19 -0700

    change view function to render buttons from list

--- src/simple_muv.elm
+++ src/simple_muv.elm
@@ -1,6 +1,7 @@
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import List exposing(map)
 
 main =
    Browser.sandbox {init = init, update = update, view = view}
@@ -37,11 +38,11 @@ type Msg =
 update : Msg -> Model -> Model 
 update msg model = 
     case msg of 
-        Card n color -> case (modBy 3 n) of 
-                            0 -> { model | color1 = color, counter = (modBy 3 (n+1))}
-                            1 -> { model | color2 = color, counter = (modBy 3 (n+1))}
-                            3 -> { model | color3 = color, counter = (modBy 3 (n+1))}
-                            _ -> { model | color3 = color, counter = (modBy 3 (n+1))}
+        Card n color -> let addOne = (modBy 3 (n+1)) in case (modBy 3 n) of 
+                            0 -> { model | color1 = color, counter = addOne}
+                            1 -> { model | color2 = color, counter = addOne}
+                            3 -> { model | color3 = color, counter = addOne}
+                            _ -> { model | color3 = color, counter = addOne}
         Button status -> { model | status = status }
         
 
@@ -50,14 +51,19 @@ update msg model =
 view : Model -> Html Msg
 view model = 
    div []
-      [
-          button [ onClick (Card model.counter Red) ] [ text "Red"]
-      ,   button [ onClick (Card model.counter Purple) ] [ text "Purple"]
-      ,   button [ onClick (Card model.counter Green) ] [ text "Green"]
-      ,   button [ onClick (Button (colorTest model.color1 model.color2 model.color3)) ] [text "Check"]
-      ,   div [] [text (if model.status == Same then "Same!" else "Different!")] 
-      ]
+       (
+       (map (\color -> (button [ onClick (Card model.counter color)] [text (show color)])) [Red, Purple, Green, Red, Purple, Green]) ++
+       [div [] [button [ onClick (Button (colorTest model.color1 model.color2 model.color3)) ] [text "Check"]]] ++
+       [div [] [text (if model.status == Same then "Same!" else "Different!")]])
 
 colorTest : Color -> Color -> Color -> Status 
 colorTest c1 c2 c3 = if (c1 == c2 && c2 == c3) then Same
                                                else Different
+
+show : Color -> String
+show c = 
+    case c of 
+        Red -> "Red"
+        Purple -> "Purple"
+        Green -> "Green"
+        Blank -> "_"
