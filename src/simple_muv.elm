4dc8104ad89f9795a9a750736bd44c0dbf6bad13
Alli <wonga10@gmail.com>
2018-11-04 20:30:46 -0800

    add another attribute.
    Change model, update, and view accordingly.

--- src/simple_muv.elm
+++ src/simple_muv.elm
@@ -9,13 +9,18 @@ main =
 -------------MODEL 
 
 type alias Model = 
-   {  color1 : Color
-   ,  color2 : Color
-   ,  color3 : Color
+   {  card1 : Card
+   ,  card2 : Card
+   ,  card3 : Card
    ,  status : Status
    ,  counter : Int
    } 
 
+type alias Card = 
+    {   
+        color  : Color
+    ,   shape  : Shape 
+    }
 type Color
     = Red 
     | Green 
@@ -28,20 +33,14 @@ type Shape
     | Wave
     | NoShape
 
-type Number
-    = One
-    | Two
-    | Three
-    | NoNum
-type Attribute
-    = Color
-    | Shape
-    | Number
+type Attribute a1 a2 a3
+    = Attribute a1 a2 a3
 
-type alias Card = 
-    {   number : Number 
-    ,   color  : Color
-    ,   shape  : Shape 
+blankCard : Card
+blankCard = 
+    {
+        color = NoColor
+    ,   shape = NoShape
     }
 
 type Status 
@@ -50,45 +49,58 @@ type Status
 
 init : Model 
 init = 
-  Model NoColor NoColor NoColor Same 0
+  Model blankCard blankCard blankCard Same 0
 
 ----------- UPDATE
 
 type Msg = 
-  C Int Color | Button Status
+  C Int Color Shape | Selected Model
 
 update : Msg -> Model -> Model 
 update msg model = 
     case msg of 
-        C n color -> let addOne = (modBy 3 (n+1)) in case (modBy 3 n) of 
-                            0 -> { model | color1 = color, counter = addOne}
-                            1 -> { model | color2 = color, counter = addOne}
-                            3 -> { model | color3 = color, counter = addOne}
-                            _ -> { model | color3 = color, counter = addOne}
-        Button status -> { model | status = status }
-        
+        C n color shape -> let addOne = (modBy 3 (n+1)) in case (modBy 3 n) of 
+                            0 -> {card1 = (setColor color model.card1), card2 = model.card2, card3 = model.card3, status = model.status, counter = addOne}
+                            1 -> {card1 = model.card1, card2 = (setColor color model.card2), card3 = model.card3, status = model.status, counter = addOne}
+                            2 -> {card1 = model.card1, card2 = model.card2, card3 = (setColor color model.card3), status = model.status, counter = addOne}
+                            _ -> {card1 = model.card1, card2 = model.card2, card3 = (setColor color model.card3), status = model.status, counter = addOne}
+        Selected m -> { model | status = checkAttributes m}
 
+setColor : Color -> Card -> Card
+setColor newColor card = 
+    { card | color = newColor}
+
+checkAttributes : Model -> Status
+checkAttributes m = let colorCheck = sameAttribute m.card1.color m.card2.color m.card3.color in 
+                        let shapeCheck = sameAttribute m.card1.shape m.card2.shape m.card3.shape in 
+                                 if colorCheck && shapeCheck then Same    
+                                                             else if not colorCheck && not shapeCheck then Same 
+                                                                                                      else Different
+
+sameAttribute : a -> a -> a -> Bool
+sameAttribute a1 a2 a3 = if a1 == a2 && a2 == a3 then True else False
 
 -------------VIEW
 view : Model -> Html Msg
 view model = 
    div []
        (
-       (map (\color -> (button [ onClick (C model.counter color)] [text (show color)])) [Red, Purple, Green, Red, Purple, Green]) ++
-       [div [] [button [ onClick (Button (colorTest model.color1 model.color2 model.color3)) ] [text "Check"]]] ++
+       (map (\(color,shape) -> (button [ onClick (C model.counter color shape)] [text (showColor color ++ showShape shape)])) [(Red, Diamond), (Purple, Wave), (Green, Round), (Red, Diamond), (Purple, Wave), (Green, Round),(Green,Round)]) ++
+       [div [] [button [ onClick (Selected model) ] [text "Check"]]] ++
        [div [] [text (if model.status == Same then "Same!" else "Different!")]])
 
-colorTest : Color -> Color -> Color -> Status 
-colorTest c1 c2 c3 = if (c1 == c2 && c2 == c3) then Same
-                                               else Different
-
-sameAttribute : Attribute -> Attribute -> Attribute -> Status
-sameAttribute a1 a2 a3 = if a1 == a2 && a2 == a3 then Same else Different
-
-show : Color -> String
-show c = 
+showColor : Color -> String
+showColor c = 
     case c of 
         Red -> "Red"
         Purple -> "Purple"
         Green -> "Green"
         NoColor -> "_"
+
+showShape : Shape -> String
+showShape s = 
+    case s of
+        Round -> "Round"
+        Diamond -> "Diamond"
+        Wave -> "Wave"
+        NoShape -> "_" 
