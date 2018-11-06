3b99cc78490d7bc0c681e755996899d035b8f9c6
Alli <wonga10@gmail.com>
2018-11-05 21:16:01 -0800

    add all unique check for attributes

--- src/simple_muv.elm
+++ src/simple_muv.elm
@@ -33,8 +33,6 @@ type Shape
     | Wave
     | NoShape
 
-type Attribute a1 a2 a3
-    = Attribute a1 a2 a3
 
 blankCard : Card
 blankCard = 
@@ -49,45 +47,52 @@ type Status
 
 init : Model 
 init = 
-  Model blankCard blankCard blankCard Same 0
+  Model blankCard blankCard blankCard Same 0 
 
------------ UPDATE
+ -----------------UPDATE
 
 type Msg = 
-  C Int Color Shape | Selected Model
+  ClickedCard Int Color Shape | Selected Model
 
 update : Msg -> Model -> Model 
 update msg model = 
     case msg of 
-        C n color shape -> let addOne = (modBy 3 (n+1)) in case (modBy 3 n) of 
-                            0 -> {card1 = (setColor color model.card1), card2 = model.card2, card3 = model.card3, status = model.status, counter = addOne}
-                            1 -> {card1 = model.card1, card2 = (setColor color model.card2), card3 = model.card3, status = model.status, counter = addOne}
-                            2 -> {card1 = model.card1, card2 = model.card2, card3 = (setColor color model.card3), status = model.status, counter = addOne}
-                            _ -> {card1 = model.card1, card2 = model.card2, card3 = (setColor color model.card3), status = model.status, counter = addOne}
+        ClickedCard n c shape -> let addOne = (modBy 3 (n+1)) in case (modBy 3 n) of 
+                            0 -> {card1 = (setAttributes c shape model.card1), card2 = model.card2, card3 = model.card3, status = model.status, counter = addOne}
+                            1 -> {card1 = model.card1, card2 = (setAttributes c shape model.card2), card3 = model.card3, status = model.status, counter = addOne}
+                            2 -> {card1 = model.card1, card2 = model.card2, card3 = (setAttributes c shape model.card3), status = model.status, counter = addOne}
+                            _ -> {card1 = model.card1, card2 = model.card2, card3 = (setAttributes c shape model.card3), status = model.status, counter = addOne}
         Selected m -> { model | status = checkAttributes m}
 
-setColor : Color -> Card -> Card
-setColor newColor card = 
-    { card | color = newColor}
+setAttributes : Color -> Shape -> Card -> Card
+setAttributes newColor newShape card = 
+    { card | color = newColor, shape = newShape}
 
 checkAttributes : Model -> Status
-checkAttributes m = let colorCheck = sameAttribute m.card1.color m.card2.color m.card3.color in 
-                        let shapeCheck = sameAttribute m.card1.shape m.card2.shape m.card3.shape in 
-                                 if colorCheck && shapeCheck then Same    
-                                                             else if not colorCheck && not shapeCheck then Same 
-                                                                                                      else Different
+checkAttributes m = let sameColor = allSame m.card1.color m.card2.color m.card3.color in 
+                    let sameShape = allSame m.card1.shape m.card2.shape m.card3.shape in 
+                    let differentColor = allDifferent m.card1.color m.card2.color m.card3.color in 
+                    let differentShape = allDifferent m.card1.shape m.card2.shape m.card3.shape in 
+                            if sameColor && sameShape then Same    
+                                        else if differentColor && differentShape then Same 
+                                                                                 else Different
 
-sameAttribute : a -> a -> a -> Bool
-sameAttribute a1 a2 a3 = if a1 == a2 && a2 == a3 then True else False
+allSame : a -> a -> a -> Bool
+allSame a1 a2 a3 = if a1 == a2 && a2 == a3 then True else False
+
+allDifferent : a -> a -> a -> Bool
+allDifferent a1 a2 a3 = if a1 /= a2 && a2 /= a3 && a1 /= a3 then True else False 
 
 -------------VIEW
 view : Model -> Html Msg
 view model = 
    div []
        (
-       (map (\(color,shape) -> (button [ onClick (C model.counter color shape)] [text (showColor color ++ showShape shape)])) [(Red, Diamond), (Purple, Wave), (Green, Round), (Red, Diamond), (Purple, Wave), (Green, Round),(Green,Round)]) ++
+       (map (\(color,shape) -> (button [ onClick (ClickedCard model.counter color shape)] [text (showColor color ++ showShape shape)])) 
+                            [(Red, Round), (Purple, Diamond), (Green, Wave), (Red, Diamond), (Purple, Wave), (Green, Round),(Green,Round)]) ++
        [div [] [button [ onClick (Selected model) ] [text "Check"]]] ++
-       [div [] [text (if model.status == Same then "Same!" else "Different!")]])
+       [div [] [text (if model.status == Same then "Same!" else "Different!")]] ++
+       [div [] [text (showColor model.card1.color ++ showShape model.card1.shape ++ " 2: " ++ showColor model.card2.color ++ showShape model.card2.shape ++ " 3:" ++ showColor model.card3.color ++ showShape model.card3.shape)]])
 
 showColor : Color -> String
 showColor c = 
